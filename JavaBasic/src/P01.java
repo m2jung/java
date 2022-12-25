f0109a9734fdcf7701b2fa7e0cc02477857768b5
m2jung <104767659+m2jung@users.noreply.github.com>
2022-12-25 15:52:26 +0900

    Add files via upload

---
+++ JavaBasic/src/P01.java
@@ -0,0 +1,28 @@
+
+public class P01 {
+
+	public static void main(String[] args) {
+	 	
+		String name = "딱지";
+		String addr = "경기도 고양이 냥냥구";
+		int age = 1;
+		int year = 2021;
+		int month = 4;
+		int day = 1;
+		char male = 'M';
+		String mail = "lovecat";
+		double hight = 60.5;
+		double weight = 5.1;
+
+	
+		System.out.println("이름: " + name);
+		System.out.println("주소: " + addr);
+		System.out.println("나이: " + age + "살");
+		System.out.println("성별: " + male);
+		System.out.println("생년월일 " + year + "년" + month + "월" + day + "일");
+		System.out.println("E-mail: " + mail + "@naver.com");
+		System.out.println("키: " + hight + "cm");
+		System.out.println("몸무게: " + weight + "kg");	
+	}
+
+}
