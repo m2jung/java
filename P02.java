1a4471c16ba13aa9b1aeae034fcac7772b67c2ea
유미정 Yu Mijung <104767659+m2jung@users.noreply.github.com>
2022-12-26 08:48:08 +0900

    Scanner로 로그인 창 만들기!

--- P02.java
+++ P02.java
@@ -1,36 +1,31 @@
 import java.util.Scanner;
 
 public class P02 {
-
-	public static void main(String[] args){
 	
-		Scanner scn = new Scanner(System.in);
-		
-		//double을 이용하여 세과목의 평균 점수는?
-		
-		System.out.print("국어 점수를 입력하시오 ");
-		int a = scn.nextInt();
-		System.out.print("수학 점수를 입력하시오 ");
-		int b = scn.nextInt();
-		System.out.print("영어 점수를 입력하시오 ");
-		int c = scn.nextInt();
-		
-		double sum = (a+b+c);
-		double average = sum / 3;
-		
-		System.out.println("당신의 점수 합계는 " + sum);
-		System.out.println("당신의 평균 점수는 " + average + "입니다.");
+	public static void main(String [] args) {
 		
 		
-
+		Scanner scanner = new Scanner (System.in);
 		
+		System.out.print("ID를 입력하세요  ");
+		String id = scanner.next();
 		
+		System.out.print("PW를 입력하세요  ");
+		int pw = scanner.nextInt();
 		
+		System.out.print("mail 주소를 입력하세요 ");
+		String mail = scanner.next();
 		
+		System.out.println();
 		
+		System.out.println("당신의 ID는 " + id + "@" + mail);
+		System.out.print("비밀번호는 " + pw + " 맞습니까?");
 		
+		System.out.println();
 		
+		String a = scanner.next();
 		
+		System.out.println("축하합니다! 가입이 완료 됐습니다!");
 		
 		
 	}
